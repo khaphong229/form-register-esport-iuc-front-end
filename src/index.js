f70f67748b345529cca03bfc1c8ea95a65a1b6d4
khaphong229 <khaphong217@gmail.com>
2024-10-29 18:43:31 +0700

    set up base project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './page/Home'
+import Register from './page/Register'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: 'team',
+        element: <Register />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
